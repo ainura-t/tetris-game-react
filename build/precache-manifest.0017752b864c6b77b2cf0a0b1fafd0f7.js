2ab6003fa8e8edf1ba85e126ae0580a931df78c5
ainura-t <stredeva@gmail.com>
2021-08-13 00:59:34 -0400

    changed title

---
+++ build/precache-manifest.0017752b864c6b77b2cf0a0b1fafd0f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7961e0fbf8dcad2d45898907a214e8e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a90c22dc8a86f8d0458",
+    "url": "/static/css/main.3e9a850e.chunk.css"
+  },
+  {
+    "revision": "9f2208084be67d600d6b",
+    "url": "/static/js/2.007788d6.chunk.js"
+  },
+  {
+    "revision": "0a90c22dc8a86f8d0458",
+    "url": "/static/js/main.7920543b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/static/media/bg.2810fe6b.png"
+  }
+]);
